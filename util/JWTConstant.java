986f49238fcdba859b45ee4059b49d94121c9b70
minhsiusu <rty4560000@gmail.com>
2024-11-10 17:48:43 +0800

    Delete util directory

--- util/JWTConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.brightClean.util;
-
-//加鹽值
-public class JWTConstant {
-    public static final String SECRET = "Aeqt7zELDZnUSpy9Qr5mRNWdT3bK2ahF";
-    // static Dotenv dotenv = Dotenv.load();
-    // public static final String SECRET = dotenv.get("JWT_CONSTANT");
-}
